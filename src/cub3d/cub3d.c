fee41b3522a86cfe5809b49fb535f280e2095edc
m-zeroual <mohammed.zeroual.mz@gmail.com>
2023-06-19 19:12:11 +0100

    creat map with pixel

--- src/cub3d/cub3d.c
+++ src/cub3d/cub3d.c
@@ -34,65 +34,130 @@ void ft_count(char **map, int *lines, int *max)
 	(*max)++;
 }
 
-void ft_pixle(t_cub3d *_cub3d, char c, int i1, int j1)
+// void ft_pixle(t_cub3d *_cub3d, char c, int i1, int j1)
+// {
+// 	int i;
+// 	int j;
+
+// 	(void)i1;
+// 	(void)j1;
+// 	// (void)c;
+
+// 	i = 0;
+// 	i1 = i1 - 6;
+// 	while (i <= 39)
+// 	{
+// 		j = 0;
+// 		while (j <= 39)
+// 		{
+// 			if (c == '1')
+// 				mlx_pixel_put(_cub3d->mlx_ptr, _cub3d->mlx_win, (j1 * 40) + (j + 1), (i1 * 40) + (i + 1), 0x808080);
+// 			if (c != '1')
+// 				mlx_pixel_put(_cub3d->mlx_ptr, _cub3d->mlx_win, (j1 * 40) + (j + 1), (i1 * 40) + (i + 1), 0xF0FFFF);
+// 			mlx_pixel_put(_cub3d->mlx_ptr, _cub3d->mlx_win, (j1 * 40) + j, i1 * 40, 0x0000FF);
+// 			j++;
+// 		}
+// 		mlx_pixel_put(_cub3d->mlx_ptr, _cub3d->mlx_win, (j1 * 40) + j, (i1 * 40) + i, 0x0000FF);
+// 		i++;
+// 	}
+// }
+
+void	img_pix_put(t_cub3d *_cub3d, int x, int y, int color)
 {
-	int i;
-	int j;
+	char    *pixel;
 
-	(void)i1;
-	(void)j1;
-	// (void)c;
+    pixel = _cub3d->img.addr + (y * _cub3d->img.line_len + x * (_cub3d->img.bpp / 8));
+	*(int *)pixel = color;
+}
 
-	i = 0;
-	i1 = i1 - 6;
-	while (i <= 39)
+// void	img_pix_put(t_cub3d *_cub3d, int x, int y, int color)
+// {
+// 	char    *pixel;
+// 	int		i;
+
+// 	i = _cub3d->img.bpp - 8;
+//     pixel = _cub3d->img.addr + (y * _cub3d->img.line_len + x * (_cub3d->img.bpp / 8));
+// 	while (i >= 0)
+// 	{
+// 		/* big endian, MSB is the leftmost bit */
+// 		if (_cub3d->img.endian != 0)
+// 			*pixel++ = (color >> i) & 0xFF;
+// 		/* little endian, LSB is the leftmost bit */
+// 		else
+// 			*pixel++ = (color >> (_cub3d->img.bpp - 8 - i)) & 0xFF;
+// 		i -= 8;
+// 	}
+// }
+
+void ft_pixle(t_cub3d *_cub3d, char c, int x1, int y1)
+{
+	int y;
+	int x;
+
+	(void)x;
+	(void)x1;
+	(void)y;
+	(void)c;
+
+	y = 0;
+	y1 = y1 - 6;
+	while (y < PIXEL - 1)
 	{
-		j = 0;
-		while (j <= 39)
+		x = 0;
+		while (x < PIXEL - 1)
 		{
 			if (c == '1')
-				mlx_pixel_put(_cub3d->mlx_ptr, _cub3d->mlx_win, (j1 * 40) + (j + 1), (i1 * 40) + (i + 1), 0x808080);
-			if (c != '1')
-				mlx_pixel_put(_cub3d->mlx_ptr, _cub3d->mlx_win, (j1 * 40) + (j + 1), (i1 * 40) + (i + 1), 0xF0FFFF);
-			mlx_pixel_put(_cub3d->mlx_ptr, _cub3d->mlx_win, (j1 * 40) + j, i1 * 40, 0x0000FF);
-			j++;
+				img_pix_put(_cub3d, (x1 * PIXEL) + (x + 1), (y1 * PIXEL) + (y + 1), 0x808080);
+			else
+				img_pix_put(_cub3d, (x1 * PIXEL) + (x + 1), (y1 * PIXEL) + (y + 1), 0xF0FFFF);
+			img_pix_put(_cub3d, (x1 * PIXEL) + x, y1 * PIXEL, 0x0000FF);
+			x++;
 		}
-		mlx_pixel_put(_cub3d->mlx_ptr, _cub3d->mlx_win, (j1 * 40) + j, (i1 * 40) + i, 0x0000FF);
-		i++;
+		// if (x1 == 0)
+		// 	img_pix_put(_cub3d, (x1 * PIXEL), (y1 * PIXEL) + y, 0x0000FF);
+		img_pix_put(_cub3d, (x1 * PIXEL), (y1 * PIXEL) + y, 0x0000FF);
+		y++;
 	}
 }
 
 void ft_drow(t_cub3d *_cub3d)
 {
-	int i;
-	int j;
+	int y;
+	int x;
 
-	i = 6;
-	while (_cub3d->map[i])
+	y = 6;
+	while (_cub3d->map[y])
 	{
-		j = 0;
-		while (_cub3d->map[i][j])
+		x = 0;
+		while (_cub3d->map[y][x])
 		{
-			if (_cub3d->map[i][j] == '1')
-				ft_pixle(_cub3d, '1', i, j);
-			else if (_cub3d->map[i][j] != '1')
-				ft_pixle(_cub3d, '0', i, j);
-			j++;
+			if (_cub3d->map[y][x] == '1')
+				ft_pixle(_cub3d, '1', x, y);
+
+			else if (_cub3d->map[y][x] != '1')
+				ft_pixle(_cub3d, '0', x, y);
+			printf("%c", _cub3d->map[y][x]);
+			x++;
 		}
-		i++;
+		printf("\n");
+		y++;
 	}
 }
 
 void cub3d(t_cub3d *_cub3d)
 {
-	
-	ft_count(_cub3d->map, &_cub3d->lines, &_cub3d->max);
+	int width = 0;
+	int height = 0;
+
+	ft_count(_cub3d->map, &_cub3d->width, &_cub3d->height);
 	_cub3d->mlx_ptr = mlx_init();
-	_cub3d->mlx_win = mlx_new_window(_cub3d->mlx_ptr, _cub3d->lines * 40, _cub3d->max * 40, "cub3d");
-
+	_cub3d->mlx_win = mlx_new_window(_cub3d->mlx_ptr, _cub3d->width * PIXEL, _cub3d->height * PIXEL, "cub3d");
+	_cub3d->img.mlx_img = mlx_new_image(_cub3d->mlx_ptr, _cub3d->width * PIXEL, _cub3d->height * PIXEL);
+	_cub3d->img.addr = mlx_get_data_addr(_cub3d->img.mlx_img, &_cub3d->img.bpp, &_cub3d->img.line_len, &_cub3d->img.endian);
 	ft_drow(_cub3d);
-	
-	ft_display(_cub3d->map);
-	mlx_loop(_cub3d->mlx_ptr);
 
+	mlx_put_image_to_window(_cub3d->mlx_ptr, _cub3d->mlx_win, _cub3d->img.mlx_img, width, height);
+	
+	// ft_display(_cub3d->map);
+	mlx_loop(_cub3d->mlx_ptr);
 }
