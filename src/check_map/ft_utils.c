55ff56d48f2def17c9f54f2c2f5c5a79d5d9f66b
Mohammed zeroual <mohammedzeroual200@gmail.com>
2023-08-15 11:50:41 +0100

    fix all norm

--- src/check_map/ft_utils.c
+++ src/check_map/ft_utils.c
@@ -6,7 +6,7 @@
 /*   By: mzeroual <mzeroual@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 09:20:00 by mzeroual          #+#    #+#             */
-/*   Updated: 2023/08/14 15:10:24 by mzeroual         ###   ########.fr       */
+/*   Updated: 2023/08/15 11:50:17 by mzeroual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,27 +89,23 @@ char	**ft_read_map(char *map_name)
 	return (map);
 }
 
-char	**ft_clean_map(char **map, t_cub3d *_cub3d)
+static int	ft_clean_map_(t_cub3d *_cub3d, char **new_map, char **map)
 {
 	char	*line;
-	char	**new_map;
 	int		count[4];
 
 	count[0] = -1;
 	count[1] = 0;
 	count[2] = 0;
 	count[3] = 1;
-	new_map = malloc((ft_count_without_newline(map) + 1) * sizeof(char *));
-	if (!new_map)
-		return (ft_free_map(map), NULL);
 	while (map[++count[0]])
 	{
 		line = ft_strtrim(map[count[0]], "\t\n");
 		if (*line)
 		{
 			if (count[2] == 1)
-				return (ft_putstr_fd("Error\n\tnew line in the map.\n\
-", 2), ft_free_map(map), NULL);
+				return (ft_putstr_fd("Error\n\tnew line in the map.\n", 2), \
+				free(line), ft_free_map(new_map), ft_free_map(map), 0);
 			new_map[count[1]++] = ft_strdup(line);
 			if (count[0] > 6 && count[3] && count[3]--)
 				_cub3d->index_map = count[0];
@@ -119,6 +115,17 @@ char	**ft_clean_map(char **map, t_cub3d *_cub3d)
 		free(line);
 	}
 	new_map[count[1]] = 0;
+	return (1);
+}
+
+char	**ft_clean_map(char **map, t_cub3d *_cub3d)
+{
+	char	**new_map;
+
+	new_map = malloc((ft_count_without_newline(map) + 1) * sizeof(char *));
+	if (!new_map)
+		return (ft_free_map(map), NULL);
+	ft_clean_map_(_cub3d, new_map, map);
 	if (!ft_check6lines(new_map, _cub3d))
 		return (ft_free_map(new_map), ft_free_map(map), NULL);
 	return (new_map);
